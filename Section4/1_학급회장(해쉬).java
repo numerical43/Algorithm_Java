2afcf09aff8d2dad649d7ff5c276943cd47caf4d
numerical43 <numerical43@daum.net>
2022-11-19 18:43:59 +0900

    1. 학급 회장(해쉬)

---
+++ Section4/1_학급회장(해쉬).java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashMap<Character, Integer> map = new HashMap<>();
+        int n = Integer.parseInt(br.readLine());
+        int max = Integer.MIN_VALUE;
+        String str = br.readLine();
+        char result = 0;
+
+        for (char c : str.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        for (char key : map.keySet()) {
+            if (map.get(key) > max) {
+                max = map.get(key);
+                result = key;
+            }
+        }
+        
+        System.out.print(result);
+    }
+}
