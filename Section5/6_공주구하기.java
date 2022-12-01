3152369b106157a5d4540254ed3c33681d4feff3
numerical43 <numerical43@daum.net>
2022-12-01 18:13:25 +0900

    6. 공주 구하기

--- Section5/6_공주구하기.java
+++ Section5/6_공주구하기.java
@@ -1,5 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        Queue<Integer> queue = new LinkedList<>();
+        int answer = 0;
 
+        for (int i = 1; i <= n; i++)
+            queue.offer(i);
+
+        while (!queue.isEmpty()) {
+            for (int i = 1; i < k; i++)
+                queue.offer(queue.poll());
+            queue.poll();
+
+            if (queue.size() == 1)
+                answer = queue.poll();
+        }
+
+        System.out.print(answer);
     }
 }
