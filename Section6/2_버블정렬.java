d3d3a99e051ad47a5afcb5cf0dad7d9cba527c00
numerical43 <numerical43@daum.net>
2022-12-06 10:41:30 +0000

    2. 버블 정렬

---
+++ Section6/2_버블정렬.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < (n - 1); i++) {
+            for (int j = 0; j < (n - 1); j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i : arr)
+            sb.append(i).append(" ");
+
+        System.out.print(sb);
+    }
+}
