56e817d148a905245e3df5fa56b3444b5ffebcfa
root <root@DESKTOP-62EBGEJ>
2022-12-13 15:08:21 +0900

    9. 뮤직비디오 (결정 알고리즘)

--- Section6/9_뮤직비디오.java
+++ Section6/9_뮤직비디오.java
@@ -1,5 +1,48 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static int count(int[] arr, int mid) {
+        int count = 1;
+        int sum = 0;
+
+        for (int i : arr) {
+            if (sum + i > mid) {
+                count++;
+                sum = i;
+            }
+            else sum += i;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int answer = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) <= m) {
+                answer = mid;
+                rt = mid - 1;
+            }
+            else
+                lt = mid + 1;
+        }
+
+        System.out.print(answer);
     }
 }
